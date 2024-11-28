55fd73acec2603dc4f2dec468e84022906297866
JoshuaHetherton <22jh1930@barton.ac.uk>
2024-11-28 12:50:47 +0000

    Functions lecture

---
+++ Week_10/functions.py
@@ -0,0 +1,20 @@
+def multiply_4():
+    output=1
+    for getnumber in range(4):
+        getinput=int(input("Enter a number"))
+        output=output*getinput
+    print(output)
+multiply_4()
+
+
+# user_inputs=[]
+# for getinputs in range(4):
+#     getinput=int(input("Enter a number"))
+#     user_inputs.append(getinput)
+# def multiply_4_2():
+#     output=1
+#     for number in user_inputs:
+#         output=number*output
+#     print(output)
+
+
