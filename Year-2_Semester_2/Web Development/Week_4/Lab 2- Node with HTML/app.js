f8d7a0b52f5d59def5427847f844e292d47f4032
Joshua-Hetherton <joshuajhetherton@btinternet.com>
2026-02-20 10:29:21 +0000

    Week_4 before Jquery stuff

--- Year-2_Semester_2/Web Development/Week_4/Lab 2- Node with HTML/app.js
+++ Year-2_Semester_2/Web Development/Week_4/Lab 2- Node with HTML/app.js
@@ -2,10 +2,22 @@ const express=require("express");
 
 const app=express();
 
+const chalk=require("chalk");
+
+const path=require("path");
+
+const debug = require("debug")("app");
+
+const morgan = require("morgan");
+app.use(morgan("tiny"));
+
 app.get("/",(req,res)=>{
-    res.sendFile(path.join(__dirname, 'views/index.html'));
+    res.sendFile(path.join(__dirname, "views/index.html"));
 });
 
 app.listen(3000,()=>{
-    console.log("Server is running on port 3000");
+    debug(`Server is running on port ${chalk.green("3000")}`);
 });
+
+//node app.js
+//set DEBUG=* && node app.js
